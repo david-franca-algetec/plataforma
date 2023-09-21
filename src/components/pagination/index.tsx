1f3f5d977de2fb41f8c275a95e79aba2d25ac925
david-franca-algetec <117481634+david-franca-algetec@users.noreply.github.com>
2023-09-21 10:17:30 -0300

    fix: JSX props should not use arrow functions

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,58 +1,58 @@
-import {FC} from "react";
-import {usePagination} from "@refinedev/chakra-ui";
-import {Box, Button, HStack, IconButton} from "@chakra-ui/react";
-import {IconChevronLeft, IconChevronRight} from "@tabler/icons";
+import { FC, useCallback } from "react";
+import { usePagination } from "@refinedev/chakra-ui";
+import { Box, Button, HStack, IconButton } from "@chakra-ui/react";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons";
 
 type PaginationProps = {
-    current: number;
-    pageCount: number;
-    setCurrent: (page: number) => void;
+  current: number;
+  pageCount: number;
+  setCurrent: (page: number) => void;
 };
 
-export const Pagination: FC<PaginationProps> = ({current, pageCount, setCurrent}) => {
-    const pagination = usePagination({
-        current,
-        pageCount,
-    });
+export const Pagination: FC<PaginationProps> = ({ current, pageCount, setCurrent }) => {
+  const pagination = usePagination({
+    current,
+    pageCount,
+  });
 
-    return (
-        <Box display="flex" justifyContent="flex-end">
-            <HStack my="3" spacing="1">
-                {pagination?.prev && (
-                    <IconButton
-                        aria-label="previous page"
-                        onClick={() => setCurrent(current - 1)}
-                        disabled={!pagination?.prev}
-                        variant="outline"
-                    >
-                        <IconChevronLeft size="18"/>
-                    </IconButton>
-                )}
+  /**
+   * Increases the current page number by 1 and updates the state.
+   */
+  const addPage = useCallback(() => {
+    setCurrent(current + 1);
+  }, [current, setCurrent]);
 
-                {pagination?.items.map((page) => {
-                    if (typeof page === "string")
-                        return <span key={page}>...</span>;
+  /**
+   * Subtracts 1 from the current page and updates the state.
+   */
+  const subtractPage = useCallback(() => {
+    setCurrent(current - 1);
+  }, [current, setCurrent]);
 
-                    return (
-                        <Button
-                            key={page}
-                            onClick={() => setCurrent(page)}
-                            variant={page === current ? "solid" : "outline"}
-                        >
-                            {page}
-                        </Button>
-                    );
-                })}
-                {pagination?.next && (
-                    <IconButton
-                        aria-label="next page"
-                        onClick={() => setCurrent(current + 1)}
-                        variant="outline"
-                    >
-                        <IconChevronRight size="18"/>
-                    </IconButton>
-                )}
-            </HStack>
-        </Box>
-    );
+  return (
+    <Box display="flex" justifyContent="flex-end">
+      <HStack my="3" spacing="1">
+        {pagination?.prev && (
+          <IconButton aria-label="previous page" onClick={subtractPage} disabled={!pagination?.prev} variant="outline">
+            <IconChevronLeft size="18" />
+          </IconButton>
+        )}
+
+        {pagination?.items.map((page) => {
+          if (typeof page === "string") return <span key={page}>...</span>;
+
+          return (
+            <Button key={page} onClick={() => setCurrent(page)} variant={page === current ? "solid" : "outline"}>
+              {page}
+            </Button>
+          );
+        })}
+        {pagination?.next && (
+          <IconButton aria-label="next page" onClick={addPage} variant="outline">
+            <IconChevronRight size="18" />
+          </IconButton>
+        )}
+      </HStack>
+    </Box>
+  );
 };
--- src/components/table/ColumnFilter.tsx
+++ src/components/table/ColumnFilter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 import { HStack, IconButton, Input, Menu, MenuButton, MenuList, VStack } from "@chakra-ui/react";
 import { IconCheck, IconFilter, IconX } from "@tabler/icons";
 import type { Column } from "@tanstack/react-table";
@@ -10,42 +10,56 @@ export const ColumnFilter: FC<{ column: Column<any, any> }> = ({ column }) => {
     return null;
   }
 
+  /**
+   * Opens the column filter and sets the initial value to the current filter value.
+   */
   const open = () =>
     setState({
       value: column.getFilterValue(),
     });
 
+  /**
+   * Closes the column filter.
+   */
   const close = () => setState(null);
 
-  const change = (value: any) => setState({ value });
+  /**
+   * Updates the state with the new value from the input element.
+   * @param e - The change event from the input element.
+   */
+  const change = (e: ChangeEvent<HTMLInputElement>) => setState({ value: e.target.value });
 
+  /**
+   * Clears the filter value of the column and closes the filter dropdown.
+   */
   const clear = () => {
     column.setFilterValue(undefined);
     close();
   };
 
+  /**
+   * Saves the current filter value to the column and closes the filter modal.
+   */
   const save = () => {
     if (!state) return;
     column.setFilterValue(state.value);
     close();
   };
 
+  /**
+   * Renders the filter element for the column.
+   * If a custom filter element is provided, it will be used.
+   * Otherwise, it will render a default input element.
+   * @returns The filter element to be rendered.
+   */
   const renderFilterElement = () => {
     const FilterComponent = (column.columnDef?.meta as any)?.filterElement;
 
     if (!FilterComponent && !!state) {
-      return (
-        <Input
-          borderRadius="md"
-          size="sm"
-          autoComplete="off"
-          value={state.value}
-          onChange={(e) => change(e.target.value)}
-        />
-      );
+      return <Input borderRadius="md" size="sm" autoComplete="off" value={state.value} onChange={change} />;
     }
 
-    return <FilterComponent value={state?.value} onChange={(e: any) => change(e.target.value)} />;
+    return <FilterComponent value={state?.value} onChange={change} />;
   };
 
   return (
